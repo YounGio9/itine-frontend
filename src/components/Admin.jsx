68ccb55c00a4eaf2dd87ece4c2111f2f10b50575
MarcosYH <marcosdeimos18@gmail.com>
2023-09-05 12:58:06 +0200

    feat: add admin router

---
+++ src/components/Admin.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Articles from '../pages/admin/Articles';
+import Navbar from './admin/Navbar';
+
+const Admin = () => {
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/article" element={<Articles />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default Admin;
