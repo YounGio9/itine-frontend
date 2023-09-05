943dadf0fa52275449be02069a8dad16a8ca2bb1
MarcosYH <marcosdeimos18@gmail.com>
2023-09-05 15:07:52 +0200

    feat: add page article for admin

---
+++ src/pages/admin/Articles.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from '../../components/admin/Navbar'
+
+export default function Articles() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Article</h1>
+    </div>
+  )
+}
