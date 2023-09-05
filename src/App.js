bc64956a4afffa446acdb23ffd924e5e0b7f800a
MarcosYH <marcosdeimos18@gmail.com>
2023-09-05 13:00:08 +0200

    feat: add admin router

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import "./App.css";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Testimonial from "./pages/Testimonial";
 import Products from "./pages/Products";
 import BlogList from "./pages/BlogList";
+import Admin from "./components/Admin";
+import Layout from "./components/Layout";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Header />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/testimonial" element={<Testimonial />} />
-          <Route path="/product" element={<Products />} />
-          <Route path="/blog_list" element={<BlogList />} />
-        </Routes>
-        <Footer />
+        <Layout>
+          <Routes>
+            {/* route-client */}
+            <Route exact path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/testimonial" element={<Testimonial />} />
+            <Route path="/product" element={<Products />} />
+            <Route path="/blog_list" element={<BlogList />} />
+            {/* route-admin */}
+            <Route path="/admin/*" element={<Admin />} />
+          </Routes>
+        </Layout>
       </div>
     </Router>
   );
