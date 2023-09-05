c2e157152f7b1bc08297e47a41ae1e449d364f6b
MarcosYH <marcosdeimos18@gmail.com>
2023-09-05 15:12:04 +0200

    update component

--- src/pages/admin/Articles.jsx
+++ src/pages/admin/Articles.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
-import Navbar from '../../components/admin/Navbar'
 
 export default function Articles() {
   return (
     <div>
-      <Navbar />
       <h1>Article</h1>
     </div>
   )
