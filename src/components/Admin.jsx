0c409b7cc636f10f64a8dabc668234ec9da9f87c
MarcosYH <marcosdeimos18@gmail.com>
2023-09-05 19:11:20 +0200

    feat: add tailwind for admin page

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Articles from '../pages/admin/Articles';
 import Navbar from './admin/Navbar';
-
+import "../tailwind.css"
 const Admin = () => {
   return (
     <div>
