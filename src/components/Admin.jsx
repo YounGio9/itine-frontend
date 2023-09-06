228901357a438dfa0a40694cdeae1d06e53fb304
MarcosYH <marcosdeimos18@gmail.com>
2023-09-06 19:50:33 +0200

    feat: add route for login admin

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -3,12 +3,14 @@ import { Route, Routes } from 'react-router-dom';
 import Articles from '../pages/admin/Articles';
 import Navbar from './admin/Navbar';
 import "../tailwind.css"
+import Login from '../pages/admin/Login';
 const Admin = () => {
   return (
     <div>
       <Navbar />
       <Routes>
         <Route path="/article" element={<Articles />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
