9ce2a3d5e397ea791ddb75d855d3b07de8c56bea
MarcosYH <marcosdeimos18@gmail.com>
2023-09-06 09:57:39 +0200

    feat: data for products

---
+++ src/data/products_men.js
@@ -0,0 +1,107 @@
+import vêtements_homme from "../assets/vêtements_homme.png";
+import vêtements_homme1 from "../assets/vêtements_homme1.png";
+
+const products_men = [
+  {
+    id: 1,
+    nom: "Produit 1",
+    description: "Description du produit 1",
+    image: vêtements_homme,
+  },
+  {
+    id: 2,
+    nom: "Produit 2",
+    description: "Description du produit 2",
+    image:  vêtements_homme ,
+  },
+  {
+    id: 3,
+    nom: "Produit 3",
+    description: "Description du produit 3",
+    image: vêtements_homme ,
+  },
+  {
+    id: 3,
+    nom: "Produit 3",
+    description: "Description du produit 3",
+    image: vêtements_homme,
+  },
+{
+    id: 3,
+    nom: "Produit 3",
+    description: "Description du produit 3",
+    image: vêtements_homme,
+  },
+{
+    id: 3,
+    nom: "Produit 3",
+    description: "Description du produit 3",
+    image: vêtements_homme,
+  },
+{
+    id: 3,
+    nom: "Produit 3",
+    description: "Description du produit 3",
+    image: vêtements_homme,
+  },
+{
+    id: 3,
+    nom: "Produit 3",
+    description: "Description du produit 3",
+    image: vêtements_homme,
+  },
+];
+const products_men2 = [
+  {
+    id: 1,
+    nom: "Produit 1",
+    description: "Description du produit 1",
+    image: vêtements_homme1,
+  },
+  {
+    id: 2,
+    nom: "Produit 2",
+    description: "Description du produit 2",
+    image:  vêtements_homme1 ,
+  },
+  {
+    id: 3,
+    nom: "Produit 3",
+    description: "Description du produit 3",
+    image: vêtements_homme1 ,
+  },
+  {
+    id: 3,
+    nom: "Produit 3",
+    description: "Description du produit 3",
+    image: vêtements_homme1,
+  },
+{
+    id: 3,
+    nom: "Produit 3",
+    description: "Description du produit 3",
+    image: vêtements_homme1,
+  },
+{
+    id: 3,
+    nom: "Produit 3",
+    description: "Description du produit 3",
+    image: vêtements_homme1,
+  },
+{
+    id: 3,
+    nom: "Produit 3",
+    description: "Description du produit 3",
+    image: vêtements_homme1,
+  },
+{
+    id: 3,
+    nom: "Produit 3",
+    description: "Description du produit 3",
+    image: vêtements_homme1,
+  },
+];
+
+
+export {products_men, products_men2 }  ;
+
---
+++ src/data/products_women.js
@@ -0,0 +1,57 @@
+import vêtements_femme1 from "../assets/vêtements_femme1.png";
+import vêtements_femme2 from "../assets/vêtements_femme2.png";
+
+const products_women = [
+  {
+    id: 1,
+    nom: "Produit 1",
+    description: "Description du produit 1",
+    image: vêtements_femme1,
+  },
+  {
+    id: 2,
+    nom: "Produit 2",
+    description: "Description du produit 2",
+    image:  vêtements_femme1 ,
+  },
+  {
+    id: 3,
+    nom: "Produit 3",
+    description: "Description du produit 3",
+    image: vêtements_femme1 ,
+  },
+  {
+    id: 3,
+    nom: "Produit 3",
+    description: "Description du produit 3",
+    image: vêtements_femme1,
+  },
+{
+    id: 3,
+    nom: "Produit 3",
+    description: "Description du produit 3",
+    image: vêtements_femme1,
+  },
+{
+    id: 3,
+    nom: "Produit 3",
+    description: "Description du produit 3",
+    image: vêtements_femme1,
+  },
+{
+    id: 3,
+    nom: "Produit 3",
+    description: "Description du produit 3",
+    image: vêtements_femme1,
+  },
+{
+    id: 3,
+    nom: "Produit 3",
+    description: "Description du produit 3",
+    image: vêtements_femme1,
+  },
+];
+
+
+export default products_women;
+
