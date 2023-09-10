2de31431081e88ff469892ea83607af7d963d8f6
MarcosYH <marcosdeimos18@gmail.com>
2023-09-10 04:15:20 +0200

    feat: add register page

--- src/pages/admin/Register.jsx
+++ src/pages/admin/Register.jsx
@@ -1,6 +1,5 @@
 import React, {useState} from 'react';
 import logo from "../../assets/logo.png";
-import {Link} from "react-router-dom";
 
 export default function Register() {
   const [name, setName] = useState("")
