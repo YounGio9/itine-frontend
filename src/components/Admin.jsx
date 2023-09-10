2dcc06771bcf0a297665d1fcc2582fcb0f1ff87d
Giovani de SOUZA <giovanidesouza999@gmail.com>
2023-09-11 00:56:14 +0100

    feat: Wrap all admins routes in AuthContextProvider

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,28 +1,31 @@
-import React from 'react';
-import {Route, Routes} from 'react-router-dom';
-import Articles from '../pages/admin/Articles';
-import Navbar from './admin/Navbar';
-import "../tailwind.css"
-import Login from '../pages/admin/Login';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Articles from "../pages/admin/Articles";
+import Navbar from "./admin/Navbar";
+import "../tailwind.css";
+import Login from "../pages/admin/Login";
 import Message from "../pages/admin/Message";
 import Allproducts from "../pages/admin/Allproducts";
 import Home from "../pages/admin/Home";
 import Shop from "../pages/admin/Shop";
 import Register from "../pages/admin/Register";
+import { AuthContextProvider } from "../contexts/AuthContext";
 
 const Admin = () => {
   return (
     <div>
-      <Navbar/>
-      <Routes>
-        <Route path="/article" element={<Articles/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/message" element={<Message/>}/>
-        <Route path="/allproducts" element={<Allproducts/>}/>
-        <Route path="/home" element={<Home/>}/>
-        <Route path="/shop" element={<Shop/>}/>
-        <Route path="/register" element={<Register/>}/>
-      </Routes>
+      <Navbar />
+      <AuthContextProvider>
+        <Routes>
+          <Route path="/article" element={<Articles />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/message" element={<Message />} />
+          <Route path="/allproducts" element={<Allproducts />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </AuthContextProvider>
     </div>
   );
 };
