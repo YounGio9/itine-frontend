7e4682604592e05eef69d52d215f5b1934613bab
Giovani de SOUZA <giovanidesouza999@gmail.com>
2023-09-11 22:01:37 +0100

    feat: Use backend prod link for registration

--- src/utils/jwtInterceptor.js
+++ src/utils/jwtInterceptor.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:8000/";
+const baseUrl = "https://backend-dev-itine.onrender.com/";
 const client = axios.create({
   withCredentials: true,
   baseURL: `${baseUrl}`,
