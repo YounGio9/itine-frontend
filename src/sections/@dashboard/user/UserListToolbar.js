63ac8300792996eff331d4d92c524a884b9cbb7a
Giovani de SOUZA <giovanidesouza999@gmail.com>
2023-09-21 07:59:27 +0100

    fix: Fix bad date format

--- src/sections/@dashboard/user/UserListToolbar.js
+++ src/sections/@dashboard/user/UserListToolbar.js
@@ -56,7 +56,7 @@ export default function UserListToolbar({ numSelected, filterName, onFilterName 
         <StyledSearch
           value={filterName}
           onChange={onFilterName}
-          placeholder="Search user..."
+          placeholder="Rechercher un utilisateur..."
           startAdornment={
             <InputAdornment position="start">
               <Iconify icon="eva:search-fill" sx={{ color: 'text.disabled', width: 20, height: 20 }} />
