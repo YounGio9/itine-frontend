f17557946e5302a5fca00a0ed7a6595fb42b589d
Giovani de SOUZA <giovanidesouza999@gmail.com>
2023-09-27 17:40:36 +0100

    feat: Add auth service

---
+++ src/services/auth.service.js
@@ -0,0 +1,9 @@
+import { Get, Post } from '../utils/axios';
+
+export function login(payload) {
+  return Post('auth/login', payload);
+}
+
+export const getProfile = async () => {
+  return Get('auth/profile');
+};
